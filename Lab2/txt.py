1dab94e115bbdb56bf5a226e7afeb26ce689ca48
zhourees <zhourees@hawaii.edu>
2026-09-02 12:44:01 -1000

    integer calculation

---
+++ Lab2/txt.py
@@ -0,0 +1,7 @@
+value_entered = input("Enter a number between 1 and 100: ");
+value_as_integer = int(value_entered);
+
+valueSquared = value_as_integer ** 2;
+print("The square of the entered number is:", valueSquared);
+
+print(F"You entered: {value_as_integer}, and the square of that number is: {valueSquared}");
