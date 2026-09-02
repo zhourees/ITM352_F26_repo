05260344d959e616a85d6ae9561c06d31bf1927c
zhourees <zhourees@hawaii.edu>
2026-09-02 12:57:39 -1000

    birth year calculation

--- Lab2/Ex1.py
+++ Lab2/Ex1.py
@@ -1,7 +1,14 @@
+# This program prompts the user to enter a number between 1 and 100, calculates the square of
+# that number, and then prints both the entered number and its square.
+# Name: Reesa Zhou
+# Date: September 2, 2026
+
 value_entered = input("Enter a number between 1 and 100: ");
 value_as_integer = int(value_entered);
 
 valueSquared = value_as_integer ** 2;
+
+print("You entered:", value_as_integer);
 print("The square of the entered number is:", valueSquared);
 
 print(F"You entered: {value_as_integer}, and the square of that number is: {valueSquared}");
---
+++ Lab2/Ex2.py
@@ -0,0 +1,11 @@
+# Ask the user to enter their birth year, calculate their age based on the current year
+# and print it out.
+# Name: Reesa Zhou
+# Date: September 2, 2026
+
+birthYear = input("Enter your birth year: ");
+currentYear = 2026;
+age = currentYear - int(birthYear);
+
+print("You entered:", birthYear);
+print("Your age is: " + str(age));
