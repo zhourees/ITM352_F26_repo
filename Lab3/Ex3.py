c370f3d3394e4eb3746b5ed82d70acb695d64f96
zhourees <zhourees@hawaii.edu>
2026-09-11 13:15:58 -1000

    continue lab 3

---
+++ Lab3/Ex3.py
@@ -0,0 +1,10 @@
+# Calculates the square root of a given number
+# Name: Reesa Zhou
+# Date: September 11, 2026
+
+def squareroot(n):
+    return (float(n)) ** 0.5;
+
+inputNumber = input("Enter a number: ");
+
+print("The square root of your inputted number is:", squareroot(inputNumber));
---
+++ Lab3/HandyMath.py
@@ -0,0 +1,10 @@
+# Includes three math functions that calculates exponents, maximum, and minimum
+# Name: Reesa Zhou
+# Date: September 11, 2026
+
+def midpoint(num1, num2):
+    return ((float(num1) + float(num2)) / 2);
+
+def squareroot(n):
+    return (float(n)) ** 0.5;
+
