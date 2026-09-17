8867b6845bd88837a3f742161f6fdcb1ede291df
zhourees <zhourees@hawaii.edu>
2026-09-16 20:07:34 -1000

    added to ex1

--- Lab4/Ex1.py
+++ Lab4/Ex1.py
@@ -13,3 +13,6 @@ print(F"Your full name using an F string is: {first} {middleInitial}. {last}");
 print("Your full name using percent s is %s %s. %s" % (first, middleInitial, last));
 print("Your full name using format method is: {} {}. {}".format(first, middleInitial, last));
 print("Your full name using list joins is: " + " ".join([first, middleInitial + ".", last]));
+
+fullNameList = [first, middleInitial, last];
+print("Your full name using the unpacking method is: {} {}. {}".format(*fullNameList));
--- Lab4/Ex4.py
+++ Lab4/Ex4.py
@@ -3,7 +3,7 @@
 # Date: September 15, 2026
 
 surveyRespondents = (1012, 1035, 1021, 1053);
-surveyRespondents.append(1054);
+surveyRespondents.append(1011);
 
-surveyRespondents = surveyRespondents + (1054,)
+surveyRespondents = surveyRespondents + (1011,)
 print("Updated survey respondents:", surveyRespondents);
