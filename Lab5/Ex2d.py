46a4212a9b8e9ddb32684c0f4daee9ed62e7b35c
zhourees <zhourees@hawaii.edu>
2026-09-23 14:47:54 -1000

    fixed errors

--- Lab5/Ex2d.py
+++ Lab5/Ex2d.py
@@ -6,12 +6,12 @@ tripDurations = [1.1, 0.8, 2.5, 2.6]
 tripFares = ("$6.25", "$5.25", "$10.50", "$8.05")
 
 trips = [
-    ("duration": 1.1, "fare": 6.25),
-    ("duration": 0.8, "fare": 5.25),
-    ("duration": 2.5, "fare": 10.50),
-    ("duration": 2.6, "fare": 8.05),
+    {"duration": 1.1, "fare": 6.25},
+    {"duration": 0.8, "fare": 5.25},
+    {"duration": 2.5, "fare": 10.50},
+    {"duration": 2.6, "fare": 8.05},
 ]
 
 print(trips)
-print("The duration of the 3rd trip is:", trips[2]{"duration"}, "miles")
+print("The duration of the 3rd trip is:", trips[2]["duration"], "miles")
 print(f"The fare of the 3rd trip is: ${trips[2]["fare"]:2}")
